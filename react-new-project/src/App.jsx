626c27ed084c1691979ccd1f30d1101ce542bf12
ManmohanJangra25 <manmohanjangra2528@gmail.com>
2024-12-27 01:58:09 +0530

    Updates

--- react-new-project/src/App.jsx
+++ react-new-project/src/App.jsx
@@ -1,14 +1,39 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import InputFIelds from "./TodoInputs";
 import TodoList from "./TodoList";
 
+const todoItemsReducer = (currentTodoItems, action) => {
+  let newTodoItems = currentTodoItems;
+  
+  if(action.type === "NEW_ITEM"){
+    newTodoItems = [
+      ...currentTodoItems,
+      action.payload
+    ]
+  } else {
+    return [];
+  }
+
+  // console.log(newTodoItems);
+  
+
+  return newTodoItems;
+}
+
 const App = () => {
-  const [todoList, setTodoList] = useState([]);
+  // const [todoList, setTodoList] = useState([]);
+  const [todoList, dispatchTodoItems] = useReducer(todoItemsReducer, []);
 
   function addList(newList) {
-    setTodoList(prevList => {
-      return [...prevList, newList];
-    });
+    // setTodoList(prevList => {
+    //   return [...prevList, newList];
+    // });
+
+    const newItemAction = {
+      type: "NEW_ITEM",
+      payload: newList
+    }
+    dispatchTodoItems(newItemAction);
   };
 
   return (
@@ -20,7 +45,7 @@ const App = () => {
         </div>
         <ul className="list-group">
           {todoList.map((todoMap) => {
-            return <TodoList key={todoMap.todo_date} listText={todoMap.todo_text} listDate={todoMap.todo_date}/>;
+            return <TodoList key={Math.random()} listText={todoMap.todo_text} listDate={todoMap.todo_date}/>;
           })}
         </ul>
       </div>
