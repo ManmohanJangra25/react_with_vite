c42802b087df6640f632e242a46bb4ced1d3b9c4
ManmohanJangra25 <manmohanjangra2528@gmail.com>
2025-01-06 21:15:16 +0530

    Social Media App UI

--- react-new-project/src/App.jsx
+++ react-new-project/src/App.jsx
@@ -1,6 +1,22 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
+import "./App.css";
+
 const App = () => {
   return (
-    <div>Hello World</div>
+    <div className="app-container">
+      <Sidebar></Sidebar>
+      <div className="main-content">
+        <Header></Header>
+        <CreatePost></CreatePost>
+        <PostList></PostList>
+        <Footer></Footer>
+      </div>
+    </div>
   );
 };
 
---
+++ react-new-project/src/components/PostList.jsx
@@ -0,0 +1,15 @@
+import Post from "./Post";
+
+const PostList = () => {
+  return (
+    <div className="d-flex gap-3 flex-wrap">
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+    </div>
+  );
+};
+
+export default PostList;
