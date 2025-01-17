147aca7e063b9d6abb79d2ca2de72671176049cf
ManmohanJangra25 <manmohanjangra2528@gmail.com>
2025-01-17 19:23:05 +0530

    Updates

--- redux/redux-demo.js
+++ redux/redux-demo.js
@@ -1,3 +1,51 @@
 const redux = require("redux");
 
-console.log(redux);
+const INITIAL_VALUE = {
+  counter: 0,
+};
+
+const reducer = (store = INITIAL_VALUE, action) => {
+  let newStore = store;
+
+  if (action.type === "INCREAMENT") {
+    newStore = { counter: store.counter + 1 };
+  }
+  if (action.type === "DECREAMENT") {
+    newStore = { counter: store.counter - 1 };
+  }
+
+  if (action.type === "ADDITION") {
+    newStore = { counter: store.counter + action.payload.number };
+  }
+  if (action.type === "SUBTRACTION") {
+    newStore = { counter: store.counter - action.payload.number };
+  }
+
+  return newStore;
+};
+const store = redux.createStore(reducer);
+
+const subscriber = () => {
+  const state = store.getState();
+  console.log(state);
+};
+
+store.subscribe(subscriber);
+
+store.dispatch({ type: "INCREAMENT" });
+store.dispatch({ type: "INCREAMENT" });
+store.dispatch({ type: "INCREAMENT" });
+store.dispatch({ type: "DECREAMENT" });
+store.dispatch({ type: "DECREAMENT" });
+store.dispatch({
+  type: "ADDITION",
+  payload: {
+    number: 7,
+  },
+});
+store.dispatch({
+  type: "SUBTRACTION",
+  payload: {
+    number: 5,
+  },
+});
